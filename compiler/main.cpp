4a3e4419a0e2cf6af99c02e544aca2f6774c028c
Igormandello <igormandello@gmail.com>
2017-11-21 01:08:40 -0200

    Implements compileVariable method
    - When compileVariable is called, it checks if the order is correct and
    throws exception if it isn't

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -3,26 +3,36 @@
 #include "lexer/Lexer.h"
 #include "parser/Parser.h"
 
+using namespace std;
 int main()
 {
     Lexer  l ("C://temp//eae.txt");
     Parser p (l);
 
-    p.compileProgramInit();
-    SliceType next = l.nextSlice(false);
-    //if(next == Variable)
-    //{
-    //   p.compileVariable();
-    //   next = l.nextSlice(false);
-    //}
-    //main loop
-    //switch(next)
-    //{
-    //  Procedure: p.compileProcedure();
-    //  Function : p.compileFunction();
-    //}
-    //prox = l.nextSlice(false);
-    //loop end
+    try
+    {
+        p.compileProgramInit();
+
+        SliceType next = l.nextSlice(false);
+        if(next == Variable)
+        {
+           p.compileVariable();
+           next = l.nextSlice(false);
+        }
+        //main loop
+        //switch(next)
+        //{
+        //  Procedure: p.compileProcedure();
+        //  Function : p.compileFunction();
+        //}
+        //prox = l.nextSlice(false);
+        //loop end
+    }
+    catch (std::exception const& e)
+    {
+        cerr << "Exception: " << e.what() << endl;
+    }
+    cout << "So far, so good";
 
     return 0;
 }
--- compiler/parser/Parser.cpp
+++ compiler/parser/Parser.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Parser.h"
 
 Parser::Parser(Lexer l)
@@ -6,6 +7,40 @@ Parser::Parser(Lexer l)
 }
 
 void Parser::compileProgramInit()
+{
+
+}
+
+void Parser::compileVariable()
+{
+    SliceType next = this->lexer.nextSlice(false);
+    if (next == Variable)
+    {
+        this->lexer.nextSlice(true);
+        next = this->lexer.nextSlice(true);
+        if (next != Identifier)
+            throw std::invalid_argument("Unexpected token, expected an identifier");
+
+        next = this->lexer.nextSlice(true);
+        if (next != Colon)
+            throw std::invalid_argument("Unexpected token, expected a colon");
+
+        next = this->lexer.nextSlice(true);
+        if (next != Identifier)
+            throw std::invalid_argument("Unexpected token, expected a type identifier");
+
+        next = this->lexer.nextSlice(true);
+        if (next != Semicolon)
+            throw std::invalid_argument("Unexpected token, expected a semicolon");
+    }
+}
+
+void Parser::compileProcedure()
+{
+
+}
+
+void Parser::compileFunction()
 {
 
 }
@@ -26,6 +61,8 @@ void Parser::compileCompoundCommand()
     next = this->lexer.nextSlice(true);
     if (next != Begin)
         throw "Missing opening";
+
+    //Commands
 
     next = this->lexer.nextSlice(true);
     if (next != End)
